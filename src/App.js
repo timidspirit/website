e76fc0bb01f973df376ab6871212354373a82d53
Ryan <ryankoec@pm.me>
2024-07-02 08:31:48 -0700

    Skills update 1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Modal from 'react-modal';
 import Header from './components/Header/Header';
 import About from './components/About/About';
-import Skills from './Skills/Skills';
+import Skills from './components/Skills/Skills';
 import Portfolio from './components/Portfolio/Portfolio';
 import Photos from './components/Photos/Photos';
 import Contact from './components/Contact/Contact';
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,20 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+
+// Mock static assets
+jest.mock('../images/html-logo.png', () => 'test-file-stub');
+jest.mock('../images/flutter-logo.png', () => 'test-file-stub');
+jest.mock('../images/css-logo.png', () => 'test-file-stub');
+jest.mock('../images/tailwind-logo.png', () => 'test-file-stub');
+jest.mock('../images/python-logo.png', () => 'test-file-stub');
+jest.mock('../images/dart-logo.png', () => 'test-file-stub');
+jest.mock('../images/mysql-logo.png', () => 'test-file-stub');
+jest.mock('../images/postgres-logo.png', () => 'test-file-stub');
+jest.mock('../images/github-logo.png', () => 'test-file-stub');
+jest.mock('../images/docker-logo.png', () => 'test-file-stub');
+jest.mock('../images/vscode-logo.png', () => 'test-file-stub');
+jest.mock('../images/postman-logo.png', () => 'test-file-stub');
+jest.mock('../images/nodejs-logo.png', () => 'test-file-stub');
+jest.mock('../images/reactjs-logo.png', () => 'test-file-stub');
